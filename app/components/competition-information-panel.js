b6baeeec331f54fdffbf82a03618ad25fb21b10c
Solomon <solomonbroadbentnz@gmail.com>
2018-03-10 11:24:11 +1300

    Added start of UI logic for edit mode in the competition information panel

--- app/components/competition-information-panel.js
+++ app/components/competition-information-panel.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  inEditMode: false,
+  notInEditMode: Ember.computed.not('inEditMode'),
+  actions: {
+    toggleEditMode() {
+      this.toggleProperty('inEditMode');
+    },
+  },
 });
