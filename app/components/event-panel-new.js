a0a63da3c2641ad3fc498760cfa852971a55701a
Solomon <solomonbroadbentnz@gmail.com>
2018-03-13 09:57:21 +1300

    Modified variable names

--- app/components/event-panel-new.js
+++ app/components/event-panel-new.js
@@ -1,41 +1,41 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  selectedEventTypeID: 'mostWeight',
+  selectedEventTypeTagName: 'mostWeight',
   eventName: 'New Event Name',
   events: [],
   eventTypes: [
-    {ID: 'mostWeight', name: 'Most Weight',},
-    {ID: 'furthestDistance', name: 'Furthest Distance',},
-    {ID: 'mostTime', name: 'Most Time',},
-    {ID: 'leastTime', name: 'Least Time',},
-    {ID: 'split', name: 'Split',},
-    {ID: 'mostRepetitions', name: 'Most Repetitions'},
+    {tagName: 'mostWeight', name: 'Most Weight',},
+    {tagName: 'furthestDistance', name: 'Furthest Distance',},
+    {tagName: 'mostTime', name: 'Most Time',},
+    {tagName: 'leastTime', name: 'Least Time',},
+    {tagName: 'split', name: 'Split',},
+    {tagName: 'mostRepetitions', name: 'Most Repetitions'},
   ],
-  selectedEvent: Ember.computed('selectedEventTypeID', function() {
+  selectedEvent: Ember.computed('selectedEventTypeTagName', function() {
     return this.get('eventTypes').filter((event) => {
-      if (event.ID === (this.get('selectedEventTypeID'))) return event;
+      if (event.tagName === (this.get('selectedEventTypeTagName'))) return event;
     })[0];
   }),
   selectedEventName: Ember.computed('selectedEvent', function() {
     return this.get('selectedEvent').name;
   }),
-  selectedEventTypeIDIsValid: Ember.computed('selectedEventTypeID', function () {
+  selectedEventTypeTagNameIsValid: Ember.computed('selectedEventTypeTagName', function () {
     let eventTypes = this.get('eventTypes');
     //TODO: Refactor this with better stream techniques
-    let matchingEventType = eventTypes.filter(event => event.ID === this.get('selectedEventTypeID')).map(e => e.ID);
+    let matchingEventType = eventTypes.filter(event => event.tagName === this.get('selectedEventTypeTagName')).map(e => e.tagName);
     return matchingEventType !== undefined;
   }),
-  newEventIsReady: Ember.computed('eventName', 'selectedEventTypeID', function() {
+  newEventIsReady: Ember.computed('eventName', 'selectedEventTypeTagName', function() {
     let newEventName = this.get('selectedEventName');
     return (newEventName !== null) && (newEventName !== undefined);
   }),
   actions: {
-    setSelectedEventType(eventID) {
+    setSelectedEventType(eventTagName) {
       //TODO: Find a way to implement this with just a function instead of a computed property
-      //The event needs to be set first as the selectedEventTypeIDIsValid relies on the field being set
-      this.set('selectedEventTypeID', eventID);
-      // if (this.get('selectedEventTypeIDIsValid') === true) this.set('selectedEventTypeID', eventID);
+      //The event needs to be set first as the selectedEventTypeTagNameIsValid relies on the field being set
+      this.set('selectedEventTypeTagName', eventTagName);
+      // if (this.get('selectedEventTypeTagNameIsValid') === true) this.set('selectedEventTypeTagName', eventTagName);
     },
     addNewEvent() {
       if (this.get('newEventIsReady') === true) {
