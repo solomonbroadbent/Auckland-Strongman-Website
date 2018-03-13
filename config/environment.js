53ef2ded4d592596d64cb27106152bf23d625d5e
Solomon <solomonbroadbentnz@gmail.com>
2018-03-14 05:53:17 +1300

    Added event detailsAdded event detailsAdded event

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,23 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: 'AIzaSyA0fM48-OnVVt9A0KAi3X0Jch7sPVkjTGQ',
+      authDomain: 'auckland-strongman-website.firebaseapp.com',
+      databaseURL: 'https://auckland-strongman-website.firebaseio.com',
+      projectId: 'auckland-strongman-website',
+      storageBucket: 'auckland-strongman-website.appspot.com',
+      messagingSenderId: '533895325095'
+    },
+
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
