519bc75dcd36098bb637ddef15b1e1598219dc81
Solomon <solomonbroadbentnz@gmail.com>
2018-03-19 07:57:49 +1300

    Added start of functionality to send user to a certain competition

--- app/components/competition-list-interface-row.js
+++ app/components/competition-list-interface-row.js
@@ -7,9 +7,13 @@ export default Component.extend({
   competitionStartDate: undefined,
   competitionEndDate: undefined,
   competitionName: undefined,
+  competitionID: undefined,
   actions: {
     toggleEditMode() {
       this.toggleProperty('inEditMode');
+    },
+    sendUserToCompetition() {
+      alert(`sending user to edit competition: ${this.get('competitionID')}`)
     }
   }
 });
