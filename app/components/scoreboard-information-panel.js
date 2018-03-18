5e88dbd3e44bfc0e03cb2d83b5f7a5bbffb86f9c
Solomon <solomonbroadbentnz@gmail.com>
2018-03-19 03:11:21 +1300

    Added saveAthlete function to controller

--- app/components/scoreboard-information-panel.js
+++ app/components/scoreboard-information-panel.js
@@ -3,9 +3,22 @@ import Component from '@ember/component';
 export default Component.extend({
   inEditMode: false,
   notInEditMode: Ember.computed.not('inEditMode'),
+  newAthleteName: '',
+  newAthleteWeight: 0,
   actions: {
     toggleEditMode() {
       this.toggleProperty('inEditMode');
+    },
+    saveAthlete() {
+      if (this.get('newAthleteName') !== '' && this.get('newAthleteWeight') > 0) {
+        const athleteName = this.get('newAthleteName');
+        const athleteWeight = this.get('newAthleteWeight');
+        const newAthlete = this.store.createRecord('athlete', {
+          name: athleteName,
+          weight: athleteWeight,
+        });
+        newAthlete.save();
+      }
     },
   },
 });
