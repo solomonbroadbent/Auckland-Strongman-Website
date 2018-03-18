e9eb4dfcc35715fb43591c0bd4cc4333ce49a651
Solomon <solomonbroadbentnz@gmail.com>
2018-03-19 07:06:15 +1300

    Added functionality to save new athletes to array

--- app/components/scoreboard-information-panel.js
+++ app/components/scoreboard-information-panel.js
@@ -3,9 +3,10 @@ import Component from '@ember/component';
 export default Component.extend({
   inEditMode: false,
   notInEditMode: Ember.computed.not('inEditMode'),
-  newAthleteName: 'a',
-  newAthleteWeight: 0,
-  rows: [{athlete: {name: 'a', weight: 0}, points: 0, position: '1st'}, {athlete: {name: 'a', weight: 0}, points: 0, position: '1st'}],
+  newAthleteName: undefined,
+  newAthleteWeight: undefined,
+  // rows: [{athlete: {name: 'a', weight: 0}, points: 0, position: '1st'}, {athlete: {name: 'a', weight: 0}, points: 0, position: '1st'}],
+  rows: [],
   actions: {
     toggleEditMode() {
       this.toggleProperty('inEditMode');
@@ -14,11 +15,19 @@ export default Component.extend({
       if (this.get('newAthleteName') !== '' && this.get('newAthleteWeight') > 0) {
         const athleteName = this.get('newAthleteName');
         const athleteWeight = this.get('newAthleteWeight');
-        const newAthlete = this.store.createRecord('athlete', {
+        /*const newAthlete = this.store.createRecord('athlete', {
           name: athleteName,
           weight: athleteWeight,
         });
-        newAthlete.save();
+        newAthlete.save();*/
+        this.get('rows').pushObject({
+          athlete: {
+            name: athleteName,
+            weight: athleteWeight,
+          },
+          points: undefined,
+          position: undefined,
+        });
       }
     },
   },
