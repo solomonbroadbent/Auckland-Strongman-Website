b2bcfd19b3f8de3bb82a96e81958ebd0b3f330f9
Solomon <solomonbroadbentnz@gmail.com>
2018-04-06 10:05:11 +1200

    Modified day adding to save correctly to data base

--- app/components/day-switcher.js
+++ app/components/day-switcher.js
@@ -3,30 +3,23 @@ import Component from '@ember/component';
 export default Component.extend({
   // TODO: Refactor so that store injection isn't needed
   store: Ember.inject.service('store'),
-  /*days: Ember.computed('competition' ,function() {
-    return this.get('store').find('competition', this.get('competition').id).then((competition) => competition.days);
-  }),*/
-  nextNumber: 1,
+  days: Ember.computed('competition' ,function() {
+    // return this.get('store').find('competition', this.get('competition').id).then((competition) => competition.days);
+    return this.get('competition').get('days');
+  }),
+  nextNumber: 0,
   competition: undefined,
   actions: {
     addNewDay() {
-      // this.get('days').then((days) => console.log(days));
-      /*this.get('days').then(function (a) {
-        console.log(a.days);
-      });*/
       let day = this.get('store').createRecord('day', {
         date: '15/11/1998',
         competition: this.get('competition'),
-        number: this.get('nextNumber')
+        // Increment property increments the nextNumber and returns it in a prefix style manner
+        number: this.incrementProperty('nextNumber'),
       });
-      day.save().then(function (day) {
-        this.get('competition').days.pushObject(day).then(function () {
-          this.get('competition').save();
-        });
-      });
-      // this.get('competition').pushObject(newDay);
-      // this.get('days').pushObject({number: this.get('nextNumber')});
-      this.incrementProperty('nextNumber');
+      let competition = this.get('competition');
+      competition.get('days').addObject(day);
+      day.save().then(competition.save());
     },
   },
 });
--- app/models/competition.js
+++ app/models/competition.js
@@ -4,12 +4,24 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   refereeName: DS.attr('string'),
   scorerName: DS.attr('string'),
-  days: DS.hasMany('day'),
-  athletes: DS.hasMany('athlete'),
-  startDate: Ember.computed('days', function() {
+  days: DS.hasMany('day', {
+    // async: true tells the adapter to immediately fetch the record from the Ember Data store, or look up the
+    //  record in Firebase
+    async: true,
+    // I'm pretty sure setting the inverse to this here defines that the days in this days relationship may only
+    //  belong to one this competition. https://stackoverflow.com/questions/25693518/ember-understanding-inverse-relationships
+    inverse: this,
+  }),
+  athletes: DS.hasMany('athlete', {
+    async: true,
+    // I'm pretty sure setting the inverse to null here means the athletes can belong to other competitions as well.
+    //  https://stackoverflow.com/questions/25693518/ember-understanding-inverse-relationships
+    inverse: null,
+  }),
+  startDate: Ember.computed('days', function () {
     return 'start date';
   }),
-  endDate: Ember.computed('days', function() {
+  endDate: Ember.computed('days', function () {
     return 'end date';
   }),
 });
