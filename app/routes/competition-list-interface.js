66847a894241e713ee4e2309b6ef634840c74f2a
Solomon <solomonbroadbentnz@gmail.com>
2018-04-05 12:34:18 +1200

    Added some changes after updating to ember v3.0.0 and adding model logic to competition-list-interface route

--- app/routes/competition-list-interface.js
+++ app/routes/competition-list-interface.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return this.store.findAll('competition');
+  },
+  /*actions: {
+    openCompetitionForEditing(competitionID) {
+      this.transitionTo('data-entry-interface');
+    }
+  }*/
 });
