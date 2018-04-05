67c7df32dc87a4f5a09e4bc6658a4ce55f2958e3
Solomon <solomonbroadbentnz@gmail.com>
2018-04-05 14:00:20 +1200

    Added ability to update competition details with competition-information-panel

--- app/components/competition-information-panel.js
+++ app/components/competition-information-panel.js
@@ -3,9 +3,13 @@ import Component from '@ember/component';
 export default Component.extend({
   inEditMode: false,
   notInEditMode: Ember.computed.not('inEditMode'),
+  competition: undefined,
   actions: {
     toggleEditMode() {
       this.toggleProperty('inEditMode');
     },
+    saveCompetition() {
+      this.get('competition').save();
+    }
   },
 });
