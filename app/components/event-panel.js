f164d1c073111e0b82ab2ee6030b1cf75c424d1e
Solomon <solomonbroadbentnz@gmail.com>
2018-04-06 11:54:47 +1200

    Added start of logic for viewing competition events and their information

--- app/components/event-panel.js
+++ app/components/event-panel.js
@@ -3,10 +3,15 @@ import Component from '@ember/component';
 export default Component.extend({
   inEditMode: false,
   notInEditMode: Ember.computed.not('inEditMode'),
+  inDataEntryMode: false,
+  notInDataEntryMode: Ember.computed.not('inDataEntryMode'),
+  event: undefined,
   actions: {
     toggleEditMode() {
       this.toggleProperty('inEditMode');
     },
+    toggleDataEntryMode() {
+      this.toggleProperty('inDataEntryMode');
+    },
   },
-  day: undefined,
 });
--- app/routes/data-entry-interface.js
+++ app/routes/data-entry-interface.js
@@ -1,7 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // TODO: Check the correct way to return multiple values from a model function 06/04/2018
   model(params) {
-    return this.store.findRecord('competition', params.competition_ID);
+    let competition = this.store.findRecord('competition', params.competition_ID);
+    // TODO: Fix the ISSUE HERE where the days aren't being returned for some reason 06/04/2018
+    let days = competition.get('days');
+    return {
+      competition: competition,
+      days: days,
+    }
   }
 });
