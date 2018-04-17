6ee460d09f4e4bad37a6da169f33c4296a4d62e5
Solomon <solomonbroadbentnz@gmail.com>
2018-04-17 15:24:00 +1200

    Added ability to edit the event names and save them to the database

--- app/components/event-panel.js
+++ app/components/event-panel.js
@@ -1,17 +1,30 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  store: Ember.inject.service('store'),
   inEditMode: false,
   notInEditMode: Ember.computed.not('inEditMode'),
   inDataEntryMode: false,
   notInDataEntryMode: Ember.computed.not('inDataEntryMode'),
   event: undefined,
+  // Model properties
+  eventName: undefined,
   actions: {
     toggleEditMode() {
       this.toggleProperty('inEditMode');
     },
     toggleDataEntryMode() {
       this.toggleProperty('inDataEntryMode');
+      this.send('save');
     },
+    save() {
+      // TODO: Change to the style seen in competition-list-interface-row for saving the name
+      alert(this.get('eventName'));
+      this.get('event').set('name', this.get('eventName'));
+      this.get('event').save();
+    },
+    delete() {
+      this.get('event').destroyRecord();
+    }
   },
 });
