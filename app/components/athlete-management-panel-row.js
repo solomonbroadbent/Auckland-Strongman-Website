35aaf7bac118ea30f36f6cae130331405d8c6280
Solomon <solomonbroadbentnz@gmail.com>
2018-04-18 16:58:21 +1200

    Added ability to add and remove athletes

--- app/components/athlete-management-panel-row.js
+++ app/components/athlete-management-panel-row.js
@@ -1,10 +1,29 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  store: Ember.inject.service('store'),
   tagName: '',
   inEditMode: false,
   notInEditMode: Ember.computed.not('inEditMode'),
-  athlete: {},
-  points: -1,
-  position: 'none',
+  athlete: undefined,
+  athleteName: undefined,
+  athleteWeight: undefined,
+  actions: {
+    async deleteAthlete() {
+      let athlete = await this.get('athlete');
+      let competition = await athlete.get('competition').then((competition) => {
+        return this.get('store').findRecord('competition', competition.id);
+      });
+      await competition.get('athletes').removeObject(athlete);
+      await athlete.destroyRecord();
+      athlete.save();
+      competition.save();
+    },
+    saveAthlete() {
+      let athlete = this.get('athlete');
+      athlete.set('name', this.get('athleteName'));
+      athlete.set('weight', this.get('athleteWeight'));
+      athlete.save();
+    }
+  }
 });
--- app/components/athlete-management-panel.js
+++ app/components/athlete-management-panel.js
@@ -1,33 +1,33 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  store: Ember.inject.service('store'),
   inEditMode: false,
   notInEditMode: Ember.computed.not('inEditMode'),
   newAthleteName: undefined,
   newAthleteWeight: undefined,
-  rows: [],
+  competition: undefined,
+  athletes: Ember.computed('competition', function() {
+    return this.get('competition').get('athletes');
+  }),
   actions: {
     toggleEditMode() {
       this.toggleProperty('inEditMode');
     },
     saveNewAthlete() {
       if (this.get('newAthleteName') !== '' && this.get('newAthleteWeight') > 0) {
-        const athleteName = this.get('newAthleteName');
-        const athleteWeight = this.get('newAthleteWeight');
-        /*const newAthlete = this.store.createRecord('athlete', {
-          name: athleteName,
-          weight: athleteWeight,
+        let competition = this.get('competition');
+        const newAthlete = this.get('store').createRecord('athlete', {
+          name: this.get('newAthleteName'),
+          weight: this.get('newAthleteWeight'),
+          competition: competition,
         });
-        newAthlete.save();*/
-        this.get('rows').pushObject({
-          athlete: {
-            name: athleteName,
-            weight: athleteWeight,
-          },
-          points: undefined,
-          position: undefined,
-        });
+        competition.get('athletes').addObject(newAthlete);
+        newAthlete.save().then(competition.save());
       }
     },
+    toggleEditMode() {
+      this.toggleProperty('inEditMode');
+    }
   },
 });
