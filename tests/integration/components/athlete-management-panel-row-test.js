ebe580e006b4920882e0e5134f2af9320b9dc083
Solomon <solomonbroadbentnz@gmail.com>
2018-04-18 15:52:42 +1200

    Refactored scoreboard-information-panel to become athlete-managment-panel

--- tests/integration/components/athlete-management-panel-row-test.js
+++ tests/integration/components/athlete-management-panel-row-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('scoreboard-information-panel', 'Integration | Component | scoreboard information panel', {
+moduleForComponent('athlete-management-panel-row', 'Integration | Component | scoreboard information panel row', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{scoreboard-information-panel}}`);
+  this.render(hbs`{{athlete-management-panel-row}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#scoreboard-information-panel}}
+    {{#athlete-management-panel-row}}
       template block text
-    {{/scoreboard-information-panel}}
+    {{/athlete-management-panel-row}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
--- tests/integration/components/athlete-management-panel-test.js
+++ tests/integration/components/athlete-management-panel-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('scoreboard-information-panel-row', 'Integration | Component | scoreboard information panel row', {
+moduleForComponent('athlete-management-panel', 'Integration | Component | scoreboard information panel', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{scoreboard-information-panel-row}}`);
+  this.render(hbs`{{athlete-management-panel}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#scoreboard-information-panel-row}}
+    {{#athlete-management-panel}}
       template block text
-    {{/scoreboard-information-panel-row}}
+    {{/athlete-management-panel}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
