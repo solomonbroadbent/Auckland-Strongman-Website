033572a18e0742f846e7a70fad5eed5c583729f1
Solomon <solomonbroadbentnz@gmail.com>
2018-05-22 22:54:30 +1200

    Revert "Modified all routes to redirect to login"
    This reverts commit f6052530ddacf1f00a4009daadfa483f3ca267f4.

--- app/router.js
+++ app/router.js
@@ -6,14 +6,12 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function () {
-  // This base route doesn't seem to actually effect the application
-  this.route('/');
+Router.map(function() {
   this.route('scoreboard-interface');
   this.route('competition-list-interface');
   this.route('data-entry-interface', {
     path: '/data-entry-interface/competition/:competition_ID'
-  }, function () {
+  }, function() {
     this.route('day', {
       path: '/day/:day_ID'
     });
