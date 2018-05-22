3772d7c62ce68ed97edde8674989bc534e063bcc
Solomon <solomonbroadbentnz@gmail.com>
2018-05-23 02:27:46 +1200

    Added logic to transfer the user to the login screen if they haven't signed in

--- app/routes/competition-list-interface.js
+++ app/routes/competition-list-interface.js
@@ -1,6 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel: function () {
+    // TODO: Check this is actually needed as the same logic is in the application router
+    if (!this.get('session').get('isAuthenticated')) {
+      this.transitionTo('login');
+    }
+  },
   model() {
     return this.store.findAll('competition');
   },
